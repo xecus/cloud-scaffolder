050b5e61b1c2470dfdc6b509b1055f55772f8ea5
Hiroyuki Ootaguro <hiroyuki@abeja.asia>
2017-01-09 09:55:56 +0000

    remove jwt middleware

--- api/api.go
+++ api/api.go
@@ -1,45 +1,20 @@
 package api
 
 import (
-	"github.com/StephanDollberg/go-json-rest-middleware-jwt"
 	"github.com/ant0ine/go-json-rest/rest"
 	"log"
 	"net/http"
 	"sync"
-	"time"
 )
 
-func handle_auth(w rest.ResponseWriter, r *rest.Request) {
-	w.WriteJson(map[string]string{"authed": r.Env["REMOTE_USER"].(string)})
-}
-
 func Ready() {
-	jwt_middleware := &jwt.JWTMiddleware{
-		Key:        []byte("secret key"),
-		Realm:      "jwt auth",
-		Timeout:    time.Hour,
-		MaxRefresh: time.Hour * 24,
-		Authenticator: func(userId string, password string) bool {
-			return userId == "admin" && password == "admin"
-		}}
 	api := rest.NewApi()
 	api.Use(rest.DefaultDevStack...)
-
-	api.Use(&rest.IfMiddleware{
-		Condition: func(request *rest.Request) bool {
-			return request.URL.Path != "/login"
-		},
-		IfTrue: jwt_middleware,
-	})
-
 	router, err := rest.MakeRouter(
 		rest.Get("/countries", GetAllCountries),
 		rest.Post("/countries", PostCountry),
 		rest.Get("/countries/:code", GetCountry),
 		rest.Delete("/countries/:code", DeleteCountry),
-		rest.Post("/login", jwt_middleware.LoginHandler),
-		rest.Get("/auth_test", handle_auth),
-		rest.Get("/refresh_token", jwt_middleware.RefreshHandler),
 	)
 	if err != nil {
 		log.Fatal(err)
